90e1020ed9bbfe8f33677ee64e78f12f422dfe78
lilyfengli <1149588247@qq.com>
2021-02-07 23:11:40 +0800

    第二版本

---
+++ 源.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int sum(int a, int b);
+	cout << sum(a, b) <<a<<" "<<b<< " " << endl;
+	
+	cout << sum(a, b) << a << " " << b << " "  << endl;
+	system("pause");
+	return 0;
+}
+int sum(int a, int b)
+{
+	int t = a;
+	a = b;
+	b = t;
+	return a + b;
+}
